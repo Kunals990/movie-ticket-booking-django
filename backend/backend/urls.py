193c3b509d94f38e6a68d859857499999d495a2b
Kunals990 <kunalsable9990@gmail.com>
2025-10-05 19:46:19 +0530

    Add swagger documentation

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,6 +26,19 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from movie.views import SignupView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Movie Ticket Booking API",
+      default_version='v1',
+      description="API documentation for the Movie Ticket Booking System",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,4 +48,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('api/',include('movie.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
